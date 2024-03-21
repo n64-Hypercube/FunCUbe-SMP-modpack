841488aa48b4662b80ec859de827075059938110
n=64_HyperCube <156005156+n64-Hypercube@users.noreply.github.com>
2024-03-21 17:15:55 +0100

    little update
    i think, the jei stuff and block swap

--- kubejs/client_scripts/jei.js
+++ kubejs/client_scripts/jei.js
@@ -31,28 +31,11 @@ JEIEvents.hideItems(event => {
         'thermal:fluid_tank_creative_augment', 
         'thermal:machine_efficiency_creative_augment', 
         'thermal:machine_catalyst_creative_augment', 
-        'immersiveengineering:capacitor_creative',  
-        Item.of('mekanism:jetpack_armored', '{mekData:{GasTanks:[{Tank:0b,stored:{amount:23329L,gasName:"mekanism:hydrogen"}}]}}'),
+        'immersiveengineering:capacitor_creative', 
         Item.of('botania:mana_tablet', '{creative:1b,mana:500000}'),
-        Item.of('mekanism:free_runners_armored', '{mekData:{EnergyContainers:[{Container:0b,stored:"64000"}]}}'), 
-        Item.of('mekanism:free_runners', '{mekData:{EnergyContainers:[{Container:0b,stored:"64000"}]}}'), 
-        Item.of('mekanism:meka_tool', '{mekData:{EnergyContainers:[{Container:0b,stored:"16000000"}]}}'), 
-        Item.of('mekanism:flamethrower', '{mekData:{GasTanks:[{Tank:0b,stored:{amount:24000L,gasName:"mekanism:hydrogen"}}]}}'), 
-        Item.of('mekanism:electric_bow', '{mekData:{EnergyContainers:[{Container:0b,stored:"120000"}]}}'), 
-        Item.of('mekanism:atomic_disassembler', '{mekData:{EnergyContainers:[{Container:0b,stored:"1000000"}]}}'), 
-        Item.of('botania:mana_tablet', '{mana:500000}'), 
-        Item.of('botania:mana_ring', '{mana:500000}'), 
-        Item.of('botania:mana_ring_greater', '{mana:2000000}'), 
-        Item.of('mekanism:free_runners_armored', '{mekData:{EnergyContainers:[{Container:0b,stored:"64000"}]}}'), 
-        Item.of('mekanism:scuba_tank', '{mekData:{GasTanks:[{Tank:0b,stored:{amount:24000L,gasName:"mekanism:oxygen"}}]}}'), 
-        Item.of('mekanism:basic_energy_cube', '{mekData:{EnergyContainers:[{Container:0b,stored:"4000000"}]}}'), 
-        Item.of('mekanism:advanced_energy_cube', '{mekData:{EnergyContainers:[{Container:0b,stored:"16000000"}]}}'), 
-        Item.of('mekanism:elite_energy_cube', '{mekData:{EnergyContainers:[{Container:0b,stored:"64000000"}]}}'), 
-        Item.of('mekanism:ultimate_energy_cube', '{mekData:{EnergyContainers:[{Container:0b,stored:"256000000"}]}}')]
     
-    ]
-    ])
-    
+        ]
+    ])    
 })
 
 JEIEvents.addItems(event => {
